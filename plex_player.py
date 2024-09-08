71d500e39d8e88ef6c431e6d537d13ef0504c1f8
joshorames <72105449+joshorames@users.noreply.github.com>
2024-09-08 18:20:07 -0500

    Update plex_player.py

--- plex_player.py
+++ plex_player.py
@@ -1,14 +1,43 @@
-import urequests
-import network
+import board, neopixel
+import time
+from urllib.request import Request, urlopen
+from plexapi.myplex import MyPlexAccount
+account = MyPlexAccount('', '')
+plex = account.resource('Josh comp').connect()
+print(plex.playlist('weird sounds').duration)
+playlist_duration = plex.playlist('weird sounds').duration
 
-# Connect to network
-wlan = network.WLAN(network.STA_IF)
-wlan.active(True)
+def led_circle(led, loop_num):
+        for x in range(16):
+                led[x]=(255,255,255)
+                time.sleep(1)
 
-# Fill in your network name (ssid) and password here:
-ssid = ''
-password = ''
-wlan.connect(ssid, password)
-print(wlan.ifconfig())
+playlist_dict = {
+    '1': ''
+}
 
-req = urequests.put('')
+led = neopixel.NeoPixel(board.D18, 16)
+
+led_circle(led,2)
+
+white = (255,255,255)
+led.fill(white)
+
+choice = input()
+
+if choice in playlist_dict:
+        while True:
+                req = Request(
+                        url=playlist_dict[choice],
+                        headers={'User-Agent': 'Mozilla/5.0'})
+                with urlopen(req) as response:
+                        body = response.read()
+                time.sleep(playlist_duration/1000)
+
+
+
+
+
+
+
+
